8080f2611dfc2d5a502f4588718d00c36c65c48b
Matt Edelman <mattedelman@gmail.com>
2015-12-02 22:46:14 -0800

    better patients list

--- model/patients.js
+++ model/patients.js
@@ -3,6 +3,21 @@
 module.exports = {
     patients: [
         {
+            firstName: 'Steven',
+            lastName: 'Pennebaker',
+            email: 'spennebaker@jisekihealth.com'
+        },
+        {
+            firstName: 'Ricardo',
+            lastName: 'Cantu',
+            email: 'ricardo@jisekihealth.com'
+        },
+        {
+            firstName: 'Prashant',
+            lastName: 'Mangidkar',
+            email: 'prashant@jisekihealth.com'
+        },
+        {
             firstName: 'Dean',
             lastName: 'Ashworth',
             email: 'dean@jisekihealth.com'
